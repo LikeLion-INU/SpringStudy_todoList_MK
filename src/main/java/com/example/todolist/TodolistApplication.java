5cfc8afc116ac4621b274760835818afd3630718
minky <minkyeong02@daum.net>
2024-03-17 15:35:21 +0900

    HiddenHttpMethodFilter 추가

--- src/main/java/com/example/todolist/TodolistApplication.java
+++ src/main/java/com/example/todolist/TodolistApplication.java
@@ -2,12 +2,19 @@ package com.example.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 public class TodolistApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodolistApplication.class, args);
+	}
+
+	@Bean //putmapping 과 deletemapping 사용을 위한 bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter(){
+		return new HiddenHttpMethodFilter();
 	}
 
 }
